ebc0108aedc922c14b50fb09b94942cd82f5c76f
Thiago Marques <contato.tmarques@gmail.com>
2022-08-10 19:05:11 -0300

    update Readme.MD

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
 
   backend "s3" {
-    region = "us-east-1" # AWS region
+    region = "us-east-1" # AWS bucket region
     bucket = "bucket-name" # S3 bucket name
     key    = "terraform.tfstate"
   }
--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
-variable "aws_region" {
-  type        = string
-  description = "região onde sera provisionada"
-  default     = "us-east-1"
-}
-
 variable "aws_profile" {
   type        = string
   description = "profile da CLI da aws"
   default     = "default"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "região onde sera provisionada"
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
@@ -16,13 +16,15 @@ variable "instance_type" {
   default     = "t3.small"
 }
 
+# tags que serao adicionadas a instancia EC2
 variable "instance_tags" {
   type        = map(string)
   description = "tags da instancia"
   default     = {
-    "Name" = "ec2 do patinho"
-    "projeto" = "cartalogo"
-    "owner" = "codehall"
+    "Name" = "Nome da instancia"
+    "projeto" = "nome do projeto"
+    "owner" = "zMarques"
+    # adicione mais tags se quiser 
   }
 }
 
